c36b9a11c0a63a13cc2835cbbd2d79d26d6915d1
Heli1005 <hgangwal4833@gmail.com>
2024-05-18 16:59:19 +0530

    connect mongoDB with backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+
 
 import { chats } from "./data/data.js";
+import { connectDB } from "./config/db.js";
 
-const app = express()
 dotenv.config()
+connectDB()
+const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/api/chat', (req, res) => {
     res.send(chats)
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!...`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!...`.yellow.bold.underline))
